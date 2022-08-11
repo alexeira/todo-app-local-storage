9c6884325aa98b2533dc371a59f195ff31d909a2
alexeira <alex.pincheira06@gmail.com>
2022-08-11 14:37:16 -0400

    add custom fonts to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lobster: ['Lobster', 'cursive', ...defaultTheme.fontFamily.sans],
+        inter: ['Inter', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
+
+
+
